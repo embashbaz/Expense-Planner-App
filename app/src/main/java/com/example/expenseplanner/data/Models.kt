c5744cd4cd0982cf6bf79efceb4bb9bd7391e6f2
Emery Bashige <bemeryb@gmail.com>
2021-07-02 20:09:38 +0300

    created model class

--- app/src/main/java/com/example/expenseplanner/data/Models.kt
+++ app/src/main/java/com/example/expenseplanner/data/Models.kt
@@ -1,2 +1,32 @@
 package com.example.expenseplanner.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Cart(
+    @PrimaryKey
+    val id: Float,
+
+    val type: String,
+    val status: Int,
+    val dateCreated: String,
+    val totalPrice: Double,
+
+
+
+)
+
+@Entity
+data class ItemProduct(
+    @PrimaryKey
+    val id: Float,
+    val cartId: Float,
+    val name: String,
+    val date: String,
+    val price: Double,
+    val quantity: Int,
+    val description: String
+
+
+)
