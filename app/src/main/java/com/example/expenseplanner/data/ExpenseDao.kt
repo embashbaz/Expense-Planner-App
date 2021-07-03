ae1f6c0739ea91057f119f2f8e20bba8e319f52e
Emery Bashige <bemeryb@gmail.com>
2021-07-03 16:49:30 +0300

    added a repository for the expense planner

--- app/src/main/java/com/example/expenseplanner/data/ExpenseDao.kt
+++ app/src/main/java/com/example/expenseplanner/data/ExpenseDao.kt
@@ -1,16 +1,38 @@
 package com.example.expenseplanner.data
 
-import androidx.room.Dao
-import androidx.room.Insert
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ExpenseDao {
 
     @Insert
-    fun insertExpense(cart: Cart)
+    suspend fun insertExpense(cart: Cart)
 
     @Insert
-    fun insertItemProduct(item: ItemProduct)
+    suspend fun insertItemProduct(item: ItemProduct)
+
+    @Update
+    suspend fun updateCart(cart: Cart)
+
+    @Update
+    suspend fun updateItemProduct(item: ItemProduct)
+
+    @Delete
+    suspend fun deleteCart(cart: Cart)
+
+    @Delete
+    suspend fun deleteItemProduct(item: ItemProduct)
+
+    @Query("SELECT * FROM Cart ORDER BY id")
+    fun getAllCart(): LiveData<List<Cart>>
+
+    @Query("SELECT * FROM Itemproduct WHERE cartId =:id")
+    fun getAllItemForCart(id: Float): LiveData<List<ItemProduct>>
+
+    @Query("DELETE FROM Itemproduct WHERE cartId =:id")
+    suspend fun deleteAllItemForCart(id: Float)
 
 
 
--- app/src/main/java/com/example/expenseplanner/data/Repository.kt
+++ app/src/main/java/com/example/expenseplanner/data/Repository.kt
@@ -1,4 +1,49 @@
 package com.example.expenseplanner.data
 
-class Repository {
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.flow.Flow
+
+
+class Repository(private val expenseDao: ExpenseDao) {
+
+    val allCarts = expenseDao.getAllCart()
+
+    fun getItemsForCart(id: Float): LiveData<List<ItemProduct>>{
+        return expenseDao.getAllItemForCart(id)
+    }
+
+    @WorkerThread
+    suspend fun insertExpense(cart: Cart){
+        expenseDao.insertExpense(cart)
+    }
+
+    @WorkerThread
+    suspend fun insertItemProduct(item: ItemProduct){
+        expenseDao.insertItemProduct(item)
+    }
+
+    @WorkerThread
+    suspend fun updateCart(cart: Cart){
+        expenseDao.updateCart(cart)
+    }
+
+    @WorkerThread
+    suspend fun updateItemProduct(item: ItemProduct){
+        expenseDao.updateItemProduct(item)
+    }
+
+    @WorkerThread
+    suspend fun deleteCart(cart: Cart){
+        expenseDao.deleteCart(cart)
+        expenseDao.deleteAllItemForCart(cart.id)
+    }
+
+    @WorkerThread
+    suspend fun deleteItemProduct(item: ItemProduct){
+        expenseDao.deleteItemProduct(item)
+    }
+
+
+
 }
