3ec2e4a0b56d6b52bc8879a971fdcccc1039cd2a
Emery Bashige <bemeryb@gmail.com>
2021-07-03 17:31:43 +0300

    added the viewmodel for the cart

--- app/src/main/java/com/example/expenseplanner/ui/cart/CartViewModel.kt
+++ app/src/main/java/com/example/expenseplanner/ui/cart/CartViewModel.kt
@@ -1,18 +1,33 @@
 package com.example.expenseplanner.ui.cart
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.expenseplanner.data.Cart
+import com.example.expenseplanner.data.ItemProduct
 import com.example.expenseplanner.data.Repository
 import kotlinx.coroutines.launch
 
 class CartViewModel(repository: Repository) : ViewModel() {
+    val repository = repository
 
-    fun insertCart(cart: Cart) = viewModelScope.launch{
+    fun insertItemProduct(item: ItemProduct) = viewModelScope.launch{
+        repository.insertItemProduct(item)
 
     }
 
+    fun deleteItemProduct(item: ItemProduct) = viewModelScope.launch{
+        repository.deleteItemProduct(item)
+    }
+
+    fun updateItemProduct(item: ItemProduct) = viewModelScope.launch{
+        repository.updateItemProduct(item)
+    }
+
+    fun getItemsForCart(id: Float): LiveData<List<ItemProduct>> {
+        return repository.getItemsForCart(id)
+    }
 
 
 }
